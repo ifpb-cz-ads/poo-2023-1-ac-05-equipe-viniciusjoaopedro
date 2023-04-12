d9804bc488a63d2192f7589d241850d5eefb77b3
Vinícius da Silva Ribeiro <vinic.s.ribeiro@gmail.com>
2023-04-12 10:51:56 -0300

    COrrigindo a questão BMExercicio06

--- BMExercicio06.java
+++ BMExercicio06.java
@@ -4,44 +4,32 @@ dentro de um laço enquanto for diferente de 0 (zero). */
 import java.util.Scanner;
 
 public class BMExercicio06 {
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int numero = -1;
+    public static void main(String[] args) {
+
+        int dia;
+
         do {
-            System.out.print("Informe um número entre 1 e 7: ");
-            numero = scanner.nextInt();
-            
-            if(numero != 0) {
-                String dia_da_semana;
-                
-                // Exibirá o dia da semana correspondente ao número
-                switch(numero) {
-                    case 1:
-                        dia_da_semana = "domingo";
-                        break;
-                    case 2:
-                        dia_da_semana = "segunda";
-                        break;
-                    case 3:
-                        dia_da_semana = "terça";
-                        break;
-                    case 4:
-                        dia_da_semana = "quarta";
-                        break;
-                    case 5:
-                        dia_da_semana = "quinta";
-                        break;
-                    case 6:
-                        dia_da_semana = "sexta";
-                        break;
-                    default:
-                        dia_da_semana = "sabado";
-                        break;
-                }
-                System.out.println("O " + numero + "º dia da semana eh " + dia_da_semana);
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Informe um número de 1 a 7: ");
+            dia = scan.nextInt();
+
+            switch (dia) {
+                case 1: System.out.println("Domingo.");
+                break;
+                case 2: System.out.println("Segunda-feira.");
+                break;
+                case 3: System.out.println("Terça-feira.");
+                break;
+                case 4: System.out.println("Quarta-feira.");
+                break;
+                case 5: System.out.println("Quinta-feira.");
+                break;
+                case 6: System.out.println("Sexta-feira.");
+                break;
+                case 7: System.out.println("Sábado.");
+                break;
+                default: System.out.println("Dia inválido!");
             }
-            
-        } while(numero != 0);
+        } while (dia != 0);
     }
 }
