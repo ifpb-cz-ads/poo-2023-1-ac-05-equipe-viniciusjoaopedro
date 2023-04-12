765174e7968671584dc0f217c5ad0336a31e22a4
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2023-04-12 08:56:12 -0300

    Create BMExercicio09.java

---
+++ BMExercicio09.java
@@ -0,0 +1,12 @@
+/* Escreva um programa que implemente um laço while que execute 20 ve-
+zes, imprimindo o valor da variável x que inicialmente está com valor 10. */
+
+public class BMExercicio09 {
+    public static void main(String args[]) {
+        int x = 10;
+        while (x != 30) {
+            System.out.println("Valor de X: " + x);
+            x++;
+        }
+    }
+}
