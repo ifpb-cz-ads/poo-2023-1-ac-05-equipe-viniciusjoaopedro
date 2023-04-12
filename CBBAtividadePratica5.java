29c85615b3e2366f4664acb246698658f53d3c44
Vinícius da Silva Ribeiro <59255818+Vini92@users.noreply.github.com>
2023-04-12 08:47:40 -0300

    Adicionada a resposta CBBAtividadePratica5

---
+++ CBBAtividadePratica5.java
@@ -0,0 +1,14 @@
+// Faça um programa que imprima todos os números pares de 0 a 100.
+
+public class CBBAtividadePratica5 {
+  public static void main(String[] args) {
+  
+   int i;
+   
+    System.out.println("Os números pares de 0 a 100: ");
+
+    for (i=0; i<=100; i+=2) {        
+        System.out.print(i + " ");             
+    }
+  }
+}
