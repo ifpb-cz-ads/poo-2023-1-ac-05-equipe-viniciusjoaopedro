3c6bb9237748afaa525c1af7049bff748d9e2054
Vinícius da Silva Ribeiro <59255818+Vini92@users.noreply.github.com>
2023-04-12 08:50:15 -0300

    Adicionado o enunciado da questão BMExercicio06

--- BMExercicio06.java
+++ BMExercicio06.java
@@ -1,3 +1,6 @@
+/* Altere o programa do exercício anterior para ficar recebendo o número
+dentro de um laço enquanto for diferente de 0 (zero). */
+
 import java.util.Scanner;
 
 public class BMExercicio06 {
