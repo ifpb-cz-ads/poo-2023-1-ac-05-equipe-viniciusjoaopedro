5640e0cfda9796a1d9b513657953d42b832d288b
Vinícius da Silva Ribeiro <59255818+Vini92@users.noreply.github.com>
2023-04-12 08:42:21 -0300

    Adicionada a resposta CBBAtividadePratica4

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,14 @@
+// Faça um programa que imprima todos os números de 0 a 100.
+
+public class CBBAtividadePratica4 {
+  public static void main(String[] args) {
+    
+   int i;
+    
+   System.out.println("Os números de 0 a 100: ");
+
+    for (i=0; i<=100; i++) {
+        System.out.print(i + " ");
+    }
+  }
+}
