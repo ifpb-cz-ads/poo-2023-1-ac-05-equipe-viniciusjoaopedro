ab4a864719b5dcbf986b25105675707e130ca157
Vinícius da Silva Ribeiro <59255818+Vini92@users.noreply.github.com>
2023-04-12 09:05:59 -0300

    Adicionada a resposta BMExercicio10

---
+++ BMExercicio10.java
@@ -0,0 +1,13 @@
+// Reescreva o programa anterior utilizando o comando do-while.
+
+public class BMExercicio10 {
+  public static void main(String[] args) {
+    
+   int x = 10;
+
+    do {    
+      System.out.printf("%d ", x);
+      x++;
+    } while (x < 30);
+  }
+}
